d906c18a24665970c7a291914fe0006e89937491
Unknown <spencercbarber@gmail.com>
2017-09-01 16:16:18 -0600

    Jumping Scripts

--- Scripting2670/Assets/Scripts/MoveCharacter.cs
+++ Scripting2670/Assets/Scripts/MoveCharacter.cs
@@ -9,20 +9,38 @@ public class MoveCharacter : MonoBehaviour {
 	CharacterController cc;
 	Vector3 tempMove;
     public float speed = 5;
+	public float gravity = 1;
+    public float jumpHeight = 0.2f;
 
-    void Start () {
+	public int jumpCount = 2;
+
+	void Start () {
 		cc = GetComponent<CharacterController>();
 		PlayButton.Play += OnPlay;
 	}
 
 	void OnPlay(){
+		MoveInput.JumpAction = Jump;
 		MoveInput.KeyAction += Move;
 		PlayButton.Play -= OnPlay;
 	}
 
+	void Jump(){
+				
+		if(cc.isGrounded)
+		{
+			print("Grounded");
+			print("jump");
+			tempMove.y = jumpHeight;
+		}
+		
+	}
+
 	void Move (float _movement) {
+		tempMove.y -= gravity*Time.deltaTime;
 		tempMove.x = _movement*speed*Time.deltaTime;
 		print("move");
 		cc.Move(tempMove);
 	}
 }
+
--- Scripting2670/Assets/Scripts/MoveInput.cs
+++ Scripting2670/Assets/Scripts/MoveInput.cs
@@ -5,17 +5,16 @@ using System;
 public class MoveInput : MonoBehaviour {
 
 	public static Action<float> KeyAction;
-
+	public static Action JumpAction;
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			JumpAction();
+		}
+		
 		if (KeyAction != null)
 		{
 			KeyAction(Input.GetAxis("Horizontal"));
 		} 
 	}
-
-	void OnPlay ()
-	{
-		
-	}
-
 }
