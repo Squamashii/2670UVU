f81b09d4a191adef247f54aae72dff8124d8b4b9
Spencer Barber <spencercbarber@gmail.com>
2017-09-11 22:28:12 -0600

    Game - Falling Bridge, NavMesh, and Tree Falling Over

---
+++ Game Project/Bear Prototypes/Assets/Scripts/BearNavMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public class BearNavMesh : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+	public int health = 100;
+
+	void OnTriggerEnter(){
+		BearActivate.BearGo += BearChase;
+		agent = GetComponent<NavMeshAgent>();
+		}
+	
+	void BearChase()
+		{
+			print("Bear Attacking");
+			//StartCoroutine(BearCoRoutine);
+		}
+	
+
+	void Update(){
+		agent.SetDestination(target.position);
+	}
+
+	/*IEnumerator BearCoRoutine(){
+		while(health >= 1){
+		agent.SetDestination(target.position);
+		yield return new WaitForSeconds(0.0001f);
+		}
+	}*/
+	
+}
