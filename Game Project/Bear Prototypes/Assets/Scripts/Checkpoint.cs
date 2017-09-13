636f71177ea239140d6191cbf98052c270768c44
Unknown <spencercbarber@gmail.com>
2017-09-13 12:00:28 -0600

    Respawn Checkpoint  Action!!!

---
+++ Game Project/Bear Prototypes/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Checkpoint : MonoBehaviour {
+
+public static Action Checkpoint1;
+	public void OnTriggerEnter(Collider other)
+	{
+			if(other.name == "Player"){
+			Checkpoint1();
+			}
+	}
+}
