5d47a605ffd1d118b46702bd141e6f8843a93483
Unknown <spencercbarber@gmail.com>
2017-11-06 15:20:59 -0700

    Latest Game Stuff and Assets

--- Game Project/Bear Prototypes/Assets/Scripts/StaticVars.cs
+++
@@ -1,8 +0,0 @@
-﻿public class StaticVars {
-
-	public static bool weaponsEnabled = true;
-	public static float playerSpeed;
-	public static float gravity;
-	public static float waterSpeed;
-	public static float waterGravity;
-}
